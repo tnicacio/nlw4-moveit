a7f9b5b38c2b47712afcdfd339e0d82a6a51179e
tnicacio <nicacio.t92@gmail.com>
2021-02-27 16:59:35 -0300

    ChallengeBox component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Profile } from '../components/Profile';
 import styles from '../styles/pages/Home.module.css';
 import { CompletedChallenges } from '../components/CompletedChallenges';
 import { Countdown } from '../components/Countdown';
+import { ChallengeBox } from '../components/ChallengeBox';
 
 import Head from 'next/head';
 
@@ -22,7 +23,9 @@ export default function Home() {
           <CompletedChallenges />
           <Countdown />
         </div>
-        <div></div>
+        <div>
+          <ChallengeBox />
+        </div>
       </section>
     </div>
   );
