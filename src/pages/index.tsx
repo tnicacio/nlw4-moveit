a0de5eb35f3bbb0b77b852dec3f652c28f73c485
tnicacio <nicacio.t92@gmail.com>
2021-02-27 15:37:49 -0300

    CountDown button styles and structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,16 +3,24 @@ import { Profile } from '../components/Profile';
 
 import styles from '../styles/pages/Home.module.css';
 import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Countdown } from '../components/Countdown';
+
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início | move.it</title>
+      </Head>
+
       <ExperienceBar />
 
       <section>
         <div>
           <Profile />
           <CompletedChallenges />
+          <Countdown />
         </div>
         <div></div>
       </section>
